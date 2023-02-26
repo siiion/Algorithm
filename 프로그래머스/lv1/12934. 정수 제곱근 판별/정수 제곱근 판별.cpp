841d5f001f9b5e674682e5a7f524acd77a4ad7f9
siiion <112245253+siiion@users.noreply.github.com>
2023-02-26 21:40:12 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 4.09 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
@@ -5,13 +5,12 @@ using namespace std;
 
 long long solution(long long n) {
     long long answer = 0;
-    for (long long i = 0; i <= n; i++) {
-        if (n == i*i) {
-            answer = (i+1) * (i+1);
-            break;     
+    for (long long i = 1; i <= n; i++) {
+        if (i * i == n) {
+            answer = (i+1)*(i+1); 
+            break;
         }
-        else
-            answer = -1;
+        else answer = -1;
     }
     return answer;
 }
