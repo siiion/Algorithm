6d9cb470ec63491cb2b9c00b77217d10b272d124
siiion <112245253+siiion@users.noreply.github.com>
2023-02-26 20:57:00 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    for (int i = 1; ; i++) {
+        if ((i * 6) % n == 0) {
+            answer = i;
+            break;
+        }
+    }
+    return answer;
+}
