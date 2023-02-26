822bb5d096e3c0f16da4d2cc1e3f8b4a2b28e82b
siiion <112245253+siiion@users.noreply.github.com>
2023-02-26 21:41:18 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 4.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.cpp
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.cpp
@@ -4,7 +4,9 @@ using namespace std;
 int solution(int n)
 {
     int answer = 0;
-    for (int i = n; i > 0; i /= 10) 
+
+    for (int i = n; i > 0; i /= 10)
         answer += i % 10;
+
     return answer;
 }
