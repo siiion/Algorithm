ad9f6721164fd5adb4615fa16011d82af0945e78
siiion <112245253+siiion@users.noreply.github.com>
2023-02-26 20:46:02 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 4.11 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string rsp) {
+    string answer = "";
+    for (int i = 0; i < rsp.length(); i++) {
+        if (rsp[i] == '2') {
+            answer += '0';
+        } else if (rsp[i] == '0') {
+            answer += '5';
+        } else {
+            answer += '2';
+        }
+    }
+    return answer;
+}
