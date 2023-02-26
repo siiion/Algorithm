f4ac5df32571bfaac843a703f0d6d78b38983676
siiion <112245253+siiion@users.noreply.github.com>
2023-02-26 21:41:34 +0900

    [level 1] Title: 평균 구하기, Time: 0.01 ms, Memory: 3.78 MB -BaekjoonHub

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.cpp
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.cpp
@@ -6,10 +6,8 @@ using namespace std;
 double solution(vector<int> arr) {
     double answer = 0;
     double sum = 0;
-    for (int i = 0; i < arr.size(); i++)
-    {
+    for (int i = 0; i < arr.size(); i++) 
         sum += arr[i];
-    }
     answer = sum / arr.size();
     return answer;
 }
