8bf975c9434cab45cd040a950744f00082ef6800
siiion <112245253+siiion@users.noreply.github.com>
2023-02-26 21:40:52 +0900

    [level 1] Title: 약수의 합, Time: 0.02 ms, Memory: 4.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.cpp
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.cpp
@@ -5,10 +5,7 @@ using namespace std;
 
 int solution(int n) {
     int answer = 0;
-    for(int i = 1; i <= n; i++)
-    {
-        if (n % i == 0)
-            answer += i;
-    }
+    for (int i = 1; i <= n; i++)
+        if (n % i == 0) answer += i;
     return answer;
 }
