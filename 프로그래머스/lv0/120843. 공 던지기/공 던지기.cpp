0bf42027f6a5e76e518fd15e133bd04938046220
siiion <112245253+siiion@users.noreply.github.com>
2023-02-26 20:44:35 +0900

    [level 0] Title: 공 던지기, Time: 0.01 ms, Memory: 3.61 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> numbers, int k) {
+    int answer = 0;
+    answer = 1;
+    for (int i = 1; i < k; i++) {
+        answer += 2;
+        if (answer > numbers.size()) {
+            answer -= numbers.size();
+        }
+    }
+    return answer;
+}
